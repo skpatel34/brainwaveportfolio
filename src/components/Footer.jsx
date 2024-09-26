f0ecd9d392c57b7997dd7ce5268225471fb213c1
smit <smitkanani45@gmail.com>
2024-09-26 11:44:05 +0530

    COMMIT

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,35 @@
-import { socials } from "../constants/iandex"
-import Section from "./Section"
+import { socials } from "../constants/iandex";
+import Section from "./Section";
 
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10 ">
       <div className="container flex sm:justify-between justify-center  items-center gap-10 max-sm:flex-col">
-        <p className="caption text-n-4 lg:block">©{new Date().getFullYear()}.All rights reserved.</p>
+        <p className="caption text-n-4 lg:block">
+          ©{new Date().getFullYear()}.All rights reserved.
+        </p>
 
         <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
-            <a href={item.url} target="_blank" className="flex items-center justify-center w-10 h-10 rounded-full bg-n-7 transition-color hover:bg-n-6 " key={item.id}>
-              <img src={item.iconUrl }
+            <a
+              href={item.url}
+              target="_blank"
+              className="flex items-center justify-center w-10 h-10 rounded-full bg-n-7 transition-color hover:bg-n-6 "
+              key={item.id}
+            >
+              <img
+                src={item.iconUrl}
                 className=""
                 width={16}
                 height={16}
-                alt={item.title} />
+                alt={item.title}
+              />
             </a>
           ))}
         </ul>
       </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
